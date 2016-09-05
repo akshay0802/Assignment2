c49f44b4c3f1c463fc6b8bdd681c6bc4fb4132ad
akshay0802 <akshayyadav0802@gmail.com>
2016-09-05 04:21:36 -0600

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,28 +1,28 @@
-# makeCacheMatrix creates a list of functions to set and get value of the matrix and
-# get, set vale of the inverse of the matrix 
+# makeCacheMatrix creates a list of functions to store the matrix and cache its inverse 
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
   set <- function(y) {
     x <<- y
     inv <<- NULL
   }
-  get <- function() x
-  setinv <- function(inverse) inv <<- inverse
-  getinv <- function() inv
-  list(set = set, get = get, setinv = setinv, getinv = getinv)
+  get <- function() x  #gets the matrix value
+  setinv <- function(inverse) inv <<- inverse  #sets inverse value of the matrix, saves cache
+  getinv <- function() inv #gets cache
+  list(set = set, get = get, setinv = setinv, getinv = getinv) # generates a list of functions
 }
 
-# cachesolve function resturns the inverse of the matrix. It checks if the inverse
-# was calculated earlier, if yes then it takes rhe result from the cache
+# cacheSolve function returns the inverse of the matrix. It checks if the inverse
+# was calculated earlier, if yes then it takes the result from the cache
+
 cacheSolve <- function(x) {
-  inv <- x$getinv()
-  if (!is.null(inv)) {
-    message("Extracting from cache")
-    return(inv)
+  inv <- x$getinv()  #get the inverse and store into "inv"
+  if (!is.null(inv)) {     #check if cache exits or not
+    message("Extracting from cache") 
+    return(inv)    #returns the inverse stores in the cache
   }
-  data <- x$get()
-  inv <- solve(data)
-  x$setinv(inv)
-  inv
+  data <- x$get()   #gets the new matrix
+  inv <- solve(data)  #performs inverse operation on the new matrix
+  x$setinv(inv) 
+  inv # returns the inverse 
 }
 
